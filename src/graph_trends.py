e67bc0275558a83ff70b899825241b2f20dfa032
STEPHEN WALD <sewald101@gmail.com>
2018-01-04 07:01:10 -0800

    major revisions, fixed yticks, ylabels, parameterized fig height, integrated palattable module, etc. tested.

--- src/graph_trends.py
+++ src/graph_trends.py
@@ -4,6 +4,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from matplotlib import rcParams
+from palettable.tableau import (Tableau_20, Tableau_10, TableauLight_10,
+        TableauMedium_10,PurpleGray_6, PurpleGray_12, ColorBlind_10, Gray_5)
+
 
 import trend_analysis as ta
 from trend_analysis import StrainSalesDF # converts single strain data to df
@@ -15,18 +18,6 @@ from trend_analysis import RankStrains # returns ranked results
 
 """Graphic design adapted from: http://www.randalolson.com/2014/06/28/how-to-make-beautiful-data-visualizations-in-python-with-matplotlib/
 """
-
-# These are the "Tableau 20" colors as RGB.
-tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),
-             (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),
-             (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),
-             (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),
-             (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]
-
-# Tableau Color Blind 10
-tableauCB10 = [(0, 107, 164), (255, 128, 14), (171, 171, 171), (89, 89, 89),
-               (95, 158, 209), (200, 82, 0), (137, 137, 137), (162, 200, 236),
-               (255, 188, 121), (207, 207, 207)]
 
 def rescale_RGB(RGB_palate):
     """Input list of RGB tuples; return list of tuples rescaled (0, 1)"""
@@ -47,7 +38,21 @@ def range_vals(df):
     return (maximum - minimum, minimum, maximum)
 
 
-def round_up(high_val, range_val):
+def select_step(data_range, max_N_ticks=10):
+    steps = [1, 10, 50, 100, 200, 250, 500, 1000, 2000, 2500, 5000, 10000]
+    for step in steps:
+        if data_range / step <= max_N_ticks:
+            return step
+
+
+def round_to_step(data_max, data_range, max_N_ticks=10):
+    """Round max or abs(min) ytick value up to next step"""
+    step = select_step(data_range, max_N_ticks)
+    multiple = (int(data_max) / step) + 1
+    return step * multiple
+
+
+def round_up_to_pwr(high_val, range_val):
     """Round up to nearest power of 10 of range_val; e.g. 9340 >> 10000"""
     power = lambda x: len(str(int(x))) - 1 # get power of 10
     r_pwr = power(range_val)
@@ -55,7 +60,7 @@ def round_up(high_val, range_val):
     return int(np.ceil(high_val / 10**(r_pwr))*10**(r_pwr))
 
 
-def round_down(low_val, range_val):
+def round_down_to_pwr(low_val, range_val):
     """Round down to nearest power of 10 of range_val; e.g. 9340 >> 9000"""
     power = lambda x: len(str(int(x))) - 1 # get power of 10
     r_pwr = digits(range_val)
@@ -63,9 +68,8 @@ def round_down(low_val, range_val):
     return int(np.floor(low_val / 10**(r_pwr))*10**(r_pwr))
 
 
-def ylims(df):
+def ylims(val_range, low, high, max_N_ticks=10):
     """Return y_limits tuple (y_low, y_high) based on df value range"""
-    val_range, low, high = range_vals(df)
     digits = lambda x: len(str(int(x)))
     range_digits = digits(val_range)
     high_digits = digits(high)
@@ -74,70 +78,72 @@ def ylims(df):
     if low >= 0:
         if range_digits == high_digits:
             y_low = 0
-            y_high = round_up(high, val_range)
+            y_high = round_to_step(high, val_range, max_N_ticks)
 
         elif range_digits < high_digits:
-            y_low = round_down(low, val_range)
-            y_high = round_up(high, val_range)
+            y_low = -1 * round_to_step(abs(low), val_range, max_N_ticks)
+            y_high = round_to_step(high, val_range, max_N_ticks)
 
     if low < 0:
-        y_low = -1 * round_up(-1*low, val_range)
-        y_high = round_up(high, val_range)
+        y_low = -1 * round_to_step(abs(low), val_range, max_N_ticks)
+        y_high = round_to_step(high, val_range, max_N_ticks)
 
     return y_low, y_high
 
 
-def select_step(low, high, max_N_ticks=10):
-    steps = [1, 10, 50, 100, 200, 250, 500, 1000, 2000, 2500, 5000, 10000]
-    tick_range = high - low
-    for step in steps:
-        if tick_range / step <= max_N_ticks:
-            return step
-
-
-def space_yticks(low, high, max_N_ticks=10):
-    step = select_step(low, high, max_N_ticks)
-    if low >= 0:
-        return range(0, high + step, step)
+def space_yticks(y_low, y_high, step):
+    if y_low >= 0:
+        return range(0, y_high + step, step)
     else:
-        neg_ticks = range(0, low - step, -step)
+        neg_ticks = range(0, y_low - step, -step)
         neg_ticks.reverse()
         neg_ticks.remove(0)
-        pos_ticks = range(0, high + step, step)
+        pos_ticks = range(0, y_high + step, step)
         neg_ticks.extend(pos_ticks)
         return neg_ticks
 
 
-def PlotCompTrends(df, normed=False, palate=tableau20, max_yticks=10,
+def y_to_str(y):
+    if y >= 0:
+        return '$' + str(y)
+    else:
+        return '-$' + str(abs(y))
+
+
+def PlotCompTrends(df, fig_height=12, palate=Tableau_20, max_yticks=10,
                    write_to_file=False, file_format='jpeg'):
     """Plot time series in CompTrendsDF object
     ARGUMENTS:
      -- df: CompTrendsDF object (pandas DataFrame)
-     -- normed: If True, df contains normed data; default, data in dollars or units
+     -- fig_height: (int, default=14)
      -- palate: (list) colors in tuples of RGB values (default=tableau20)
      -- write_to_file: if True, writes plot to image file (default=False)
      -- file_format: (str) image file extension (default='jpeg')
     """
-    # plt.figure(figsize=(12,14))
-    plt.figure(figsize=(12,12))
+    plt.figure(figsize=(12,fig_height))
     ax = plt.subplot(111)
-    colors = rescale_RGB(palate)
+    colors = rescale_RGB(palate.colors)
     # remove plot frame lines
     ax.spines['top'].set_visible(False)
-    ax.spines['bottom'].set_patch_line(ls='--')
+    ax.spines['bottom'].set_visible(False)
     ax.spines['right'].set_visible(False)
     ax.spines['left'].set_visible(False)
 
     # set y_axis limits
-    if not normed:
-        ylimits = ylims(df) # custom function
-        plt.ylim(ylimits[0], ylimits[1])
+    if ('norm' or 'scale') in df.name.lower():
+        pass
+
+    else:
+        val_range, data_min, data_max = range_vals(df)
+        y_low, y_high = ylims(val_range, data_min, data_max, max_N_ticks=max_yticks) # custom function
+        step = select_step(val_range, max_N_ticks=max_yticks)
+        plt.ylim(y_low, y_high)
 
         # space and format y_ticks
-        tick_arr = space_yticks(ylimits[0], ylimits[1], max_yticks)
+        tick_arr = space_yticks(y_low, y_high, step)
         plt.tick_params(axis='y', which='both', bottom='off', top='off',
                         left='off', right='off', labelleft='on')
-        plt.yticks(tick_arr, ["$" + str(x) for x in tick_arr], fontsize=14)
+        plt.yticks(tick_arr, [y_to_str(y) for y in tick_arr], fontsize=14)
         ax.yaxis.grid(True, ls='--')
 
     # set x_axis limits; add one day to upper limit (for tick mark)
@@ -159,10 +165,12 @@ def PlotCompTrends(df, normed=False, palate=tableau20, max_yticks=10,
     # plot data
     strains = list(df.columns)
     for i, strain in enumerate(strains):
-        plt.plot(df[strain], lw=2.5, color=colors[i])
+        ci = i % len(colors) # revolving index for colors
+        plt.plot(df[strain], lw=2.5, color=colors[ci])
         y_pos = df[strain].values[-1]
         plt.text(idx[-1] + pd.DateOffset(1), y_pos, strain, fontsize=14,
-                color=colors[i], va='center')
+                color=colors[ci], va='center')
+
     plt.title(df.name, fontsize=18)
     rcParams['axes.titlepad'] = 50
     plt.show()
